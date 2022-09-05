26a2afc0eb13f45d01306092bd9a0819034743c1
wb1110 <williamjaredbuchanan@gmail.com>
2022-09-05 15:41:03 -0500

    gender back button

--- screens/assessment/Gender.js
+++ screens/assessment/Gender.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { FocusedStatusBar } from '../../components';
@@ -18,9 +18,14 @@ function Gender({ navigation }) {
         <CustomText>Fill in the following to get started:</CustomText>
         <Button title="Female" />
         <Button title="Nonbinary" />
-        <TouchableOpacity onPress={() => {navigation.navigate('HeightWeightAge')}}>
-          <Ionicons name="arrow-forward-circle" size={48} color={COLORS.primary} />
-        </TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity onPress={() => {navigation.navigate('Home')}}>
+            <Ionicons name="arrow-back-circle" size={48} color={COLORS.primary} navigation={navigation} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {navigation.navigate('HeightWeightAge')}}>
+            <Ionicons name="arrow-forward-circle" size={48} color={COLORS.primary} />
+          </TouchableOpacity>
+        </View>
       </Container>
     </SafeAreaView>
   );
