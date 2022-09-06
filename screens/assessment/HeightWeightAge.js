8f3e492afcbba33d6395bbf8906c2bfed6ae11dd
wb1110 <williamjaredbuchanan@gmail.com>
2022-09-06 05:17:37 -0500

    formik and state working

--- screens/assessment/HeightWeightAge.js
+++ screens/assessment/HeightWeightAge.js
@@ -7,6 +7,8 @@ import { FocusedStatusBar } from '../../components';
 import Container from '../../components/Container';
 import { COLORS } from '../../constants/theme';
 import useStore from '../../state/Store';
+import { Formik } from 'formik';
+
 
 
 function HeightWeightAge({ navigation }) {
@@ -14,29 +16,36 @@ function HeightWeightAge({ navigation }) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar />
-      <Container>
-        <Input label='Height' value={state.assessment.height} onChange={(e) => state.setHeight(e.target.value)}/>
-        <Input label='Weight' />
-        <Input label='Age' />
-        <Button title="Submit" buttonStyle={{
-                backgroundColor: COLORS.primary,
-                borderWidth: 2,
-                borderColor: COLORS.primary,
-                borderRadius: 30,
-              }}
-              containerStyle={{
-                width: 200,
-                marginHorizontal: 50,
-                marginVertical: 10,
-              }}
-              onPress={() => {navigation.navigate('BodyFatPercentage') }}
-          />
-        <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity onPress={() => {navigation.navigate('Gender')}}>
-            <Ionicons name="arrow-back-circle" size={48} color={COLORS.primary} navigation={navigation} />
-          </TouchableOpacity>
-        </View>
-      </Container>
+      <Formik
+        initialValues={{ height: state.assessment.height, weight: '', age: '' }}
+        onSubmit={values => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <Container>
+          <Input label='Height' value={values.height} onChangeText={handleChange('height')} onBlur={handleBlur('height')}/>
+          <Input label='Weight' />
+          <Input label='Age' />
+          <Button title="Submit" buttonStyle={{
+                  backgroundColor: COLORS.primary,
+                  borderWidth: 2,
+                  borderColor: COLORS.primary,
+                  borderRadius: 30,
+                }}
+                containerStyle={{
+                  width: 200,
+                  marginHorizontal: 50,
+                  marginVertical: 10,
+                }}
+                onPress={() => {handleSubmit; state.setHeight(values.height); navigation.navigate('BodyFatPercentage') }}
+            />
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity onPress={() => {navigation.navigate('Gender')}}>
+              <Ionicons name="arrow-back-circle" size={48} color={COLORS.primary} navigation={navigation} />
+            </TouchableOpacity>
+          </View>
+        </Container>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 }
