646cd5d0abbedc0443b0fc07ec7caec29d34bb19
wb1110 <williamjaredbuchanan@gmail.com>
2022-09-13 07:18:35 -0500

    need to work on skinfolds more

--- screens/assessment/skinFoldSites/ThreeSite.js
+++ screens/assessment/skinFoldSites/ThreeSite.js
@@ -1,13 +1,24 @@
 import { Input } from "@rneui/themed";
+import { useState } from "react";
 import Container from '../../../components/Container'
 
 function ThreeSite({ skinfolds, setSkinfolds }) {
-  const handleChange = {
-    
+  const [threeSite, setThreeSite] = useState({
+    abdominal: 0,
+    triceps: 0,
+    suprailiac: 0,
+  })
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const fieldName = e.label;
+    setThreeSite({
+      [fieldName]: value
+    })
   }
   return (
     <Container>
-      <Input label='Abdominal' />
+      <Input label='Abdominal' fieldName='abdominal' value={threeSite.abdominal} onChange={handleChange}/>
       <Input label='Triceps' />
       <Input label='Suprailiac' />
     </Container>
