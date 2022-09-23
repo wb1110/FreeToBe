6c0c36ee06a27afb97c479abaaee13f7e6201ef6
wb1110 <williamjaredbuchanan@gmail.com>
2022-09-23 16:11:15 -0500

    lie format

--- screens/assessment/Disclaimer.js
+++ screens/assessment/Disclaimer.js
@@ -1,3 +1,4 @@
+import { Text } from '@rneui/themed';
 import { Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
 import LArrowButton from '../../components/Buttons/LArrowButton';
 import RArrowButton from '../../components/Buttons/RArrowButton';
@@ -11,13 +12,19 @@ function Disclaimer({ navigation }) {
       <SafeAreaView style={{ flex: 1 }}>
         <FocusedStatusBar />
         <Container>
+          <Text h1>Disclaimer</Text>
           <TextContainer>
-            Disclaimer - If you do not know where you are, you can not know where you are going. We
-            know that tracking your food can have a negative effect on some women but to get
+            If you do not know where you are, you can not know where you are going.
+            {'\n'}
+            {'\n'}
+            We know that tracking your food can have a negative effect on some women but to get
             started, we need 3 days of your eating habits to set your goals so you never have to
-            track again. There are no restrictions or requirements for what you enter. What you eat
-            is neither good nor bad, but it will help us know how to guide you to intuitive eating
-            and food freedom.
+            track again.
+            {'\n'}
+            {'\n'}
+            There are no restrictions or requirements for what you enter. What you eat is neither
+            good nor bad, but it will help us know how to guide you to intuitive eating and food
+            freedom.
           </TextContainer>
           <View style={{ flexDirection: 'row' }}>
             <LArrowButton onPress={() => navigation.goBack()} />
--- screens/assessment/LieSkinny.js
+++ screens/assessment/LieSkinny.js
@@ -1,3 +1,4 @@
+import { Text } from '@rneui/themed';
 import { Keyboard, SafeAreaView, TouchableWithoutFeedback, View } from 'react-native';
 import LArrowButton from '../../components/Buttons/LArrowButton';
 import RArrowButton from '../../components/Buttons/RArrowButton';
@@ -12,10 +13,18 @@ function LieSkinny({ navigation }) {
         <FocusedStatusBar />
         <Container>
           <TextContainer>
-            Our society tells women eating fewer calories equals skinny beautiful women. THIS IS A
-            LIE we have believed in the last several decades. Women need nourishment, carbohydrates,
-            and joys around food to truly thrive.
+            Our society tells women eating fewer calories equals skinny beautiful women.
+            {'\n'}
+            {'\n'}
+            <Text style={{ textAlign: 'center' }} h4>
+              THIS IS A LIE!
+            </Text>
+            {'\n'}
+            {'\n'}
+            We have believed this lie the last several decades. {'\n'}
+            Women need nourishment, carbohydrates, and joys around food to truly thrive.
           </TextContainer>
+
           <View style={{ flexDirection: 'row' }}>
             <LArrowButton onPress={() => navigation.goBack()} />
             <RArrowButton title="Submit" onPress={() => navigation.navigate('LieSkinny2')} />
