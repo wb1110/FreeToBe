74194092a1226460e27b6e5191d42187ba52bb8c
wb1110 <williamjaredbuchanan@gmail.com>
2022-09-27 09:03:59 -0500

    searchbar progress

--- screens/tracker/Tracker.js
+++ screens/tracker/Tracker.js
@@ -1,9 +1,27 @@
 import { View, Text } from 'react-native';
+import { SearchBar } from '@rneui/themed';
+import { useState } from 'react';
+import FoodScanner from '../foodScanner/FoodScanner';
 
 function Tracker() {
+  const [search, setSearch] = useState('');
+
+  const updateSearch = (searchValue) => {
+    setSearch(searchValue);
+  };
   return (
     <View>
       <Text>Tracker</Text>
+      <View style={{ position: 'absolute', width: '100%', top: 400 }}>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={updateSearch}
+          value={search}
+          inputContainerStyle={{ backgroundColor: 'white' }}
+          containerStyle={{ borderBottomColor: 'transparent', borderTopColor: 'transparent' }}
+        />
+      </View>
+      <FoodScanner />
     </View>
   );
 }
