55562a9ca840bc5dac9d527ad207d955bb2dcbd5
wb1110 <williamjaredbuchanan@gmail.com>
2022-11-26 10:45:34 -0600

    need to fix scroll

--- screens/tracker/Meals.js
+++ screens/tracker/Meals.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { FAB } from '@rneui/themed';
 import { Ionicons } from '@expo/vector-icons';
 import MealItem from './MealItem';
@@ -7,25 +7,27 @@ export default function Meals({ tracker, currentIndex, navigation }) {
   const savedDate = tracker[currentIndex];
   return (
     <View style={{ flex: 5, margin: '2%', width: '100%' }}>
-      {savedDate
-        ? savedDate.meals.map((item) => (
-            <MealItem
-              mealName={item.mealName}
-              key={item.mealID}
-              mealTime={item.mealTime}
-              foodItems={item.foodItems}
-              navigation={navigation}
-              dayIndex={currentIndex}
-            />
-          ))
-        : null}
-      <FAB
-        icon={<Ionicons name="add" size={24} color="black" />}
-        size="medium"
-        color="white"
-        style={{ marginTop: '2%' }}
-        onPress={() => navigation.navigate('AddMeal', { savedDate })}
-      />
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        {savedDate
+          ? savedDate.meals.map((item) => (
+              <MealItem
+                mealName={item.mealName}
+                key={item.mealID}
+                mealTime={item.mealTime}
+                foodItems={item.foodItems}
+                navigation={navigation}
+                dayIndex={currentIndex}
+              />
+            ))
+          : null}
+        <FAB
+          icon={<Ionicons name="add" size={24} color="black" />}
+          size="medium"
+          color="white"
+          style={{ marginTop: '2%' }}
+          onPress={() => navigation.navigate('AddMeal', { savedDate })}
+        />
+      </ScrollView>
     </View>
   );
 }
