b6202c7f8a28b80109395c56b31155118ba54a42
wb1110 <williamjaredbuchanan@gmail.com>
2022-12-06 08:51:44 -0600

    settings formatting

--- screens/settings/Settings.js
+++ screens/settings/Settings.js
@@ -1,7 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button, useTheme } from '@rneui/themed';
+import { Button, Text, useTheme } from '@rneui/themed';
 import { View } from 'react-native';
-import StandardButton from '../../components/Buttons/StandardButton';
 
 export default function Settings({ navigation }) {
   const { theme } = useTheme();
@@ -53,21 +52,78 @@ export default function Settings({ navigation }) {
     // ['@MyApp_user', '@MyApp_key']
   };
   return (
-    <View style={{ justifyContent: 'center', flex: 1, backgroundColor: theme.colors.primary }}>
-      <View style={{ width: '100%' }}>
+    <View
+      style={{
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: theme.colors.primary,
+        alignItems: 'flex-end',
+      }}
+    >
+      <View
+        style={{
+          width: '95%',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomWidth: '1%',
+          borderBottomColor: theme.colors.secondary,
+        }}
+      >
         <Button title="Clear Assessment Data" onPress={() => removeAssessment()} />
+        <Text style={{ margin: '2%' }}>{'>'}</Text>
       </View>
-      <View style={{ width: '100%' }}>
+      <View
+        style={{
+          width: '95%',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomWidth: '1%',
+          borderBottomColor: theme.colors.secondary,
+        }}
+      >
         <Button title="Clear Tracker Data" onPress={() => removeTracker()} />
+        <Text style={{ margin: '2%' }}>{'>'}</Text>
       </View>
-      <View style={{ width: '100%' }}>
+      <View
+        style={{
+          width: '95%',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomWidth: '1%',
+          borderBottomColor: theme.colors.secondary,
+        }}
+      >
         <Button title="Clear Three Day Log Data" onPress={() => removeThreeDayLog()} />
+        <Text style={{ margin: '2%' }}>{'>'}</Text>
       </View>
-      <View style={{ width: '100%' }}>
+      <View
+        style={{
+          width: '95%',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomWidth: '1%',
+          borderBottomColor: theme.colors.secondary,
+        }}
+      >
         <Button title="Check Data" onPress={() => getAllKeys()} />
+        <Text style={{ margin: '2%' }}>{'>'}</Text>
       </View>
-      <View style={{ width: '100%' }}>
+      <View
+        style={{
+          width: '95%',
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderBottomWidth: '1%',
+          borderBottomColor: theme.colors.secondary,
+        }}
+      >
         <Button title="Retake Assessment" onPress={() => navigation.navigate('HeightWeightAge')} />
+        <Text style={{ margin: '2%' }}>{'>'}</Text>
       </View>
     </View>
   );
