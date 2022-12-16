7ae9938ae6604604053b5c25e9a2f9eabbd60e64
wb1110 <williamjaredbuchanan@gmail.com>
2022-12-16 13:41:11 -0600

    text color

--- screens/tracker/NutritionFacts.js
+++ screens/tracker/NutritionFacts.js
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '@rneui/themed';
 import React from 'react';
 
 function Item({ name, nutrient, multiplier }) {
