89180f47c2625e530e7d1ae0f67aade280d34fe8
wb1110 <williamjaredbuchanan@gmail.com>
2023-01-26 10:31:38 -0600

    predicted ov and period

--- screens/metabolicJournal/PeriodCalendar.js
+++ screens/metabolicJournal/PeriodCalendar.js
@@ -8,18 +8,66 @@ export default function PeriodCalendar() {
   const metabolicState = useMetabolicStore();
   const { metabolicJournal } = metabolicState;
   const [selected, setSelected] = useState(new Date());
+
+  function predictOvulationNextPeriod(data, date) {
+    const previousDate = moment(date).subtract(1, 'days').format('MM/DD/YYYY');
+    const filteredData = data.filter(
+      (item) => item.date === previousDate && item.period.menstrualFlow === ''
+    );
+    if (filteredData) {
+      const predictedPeriod = moment(date).add(28, 'days').format('YYYY-MM-DD');
+      const predictedOvulation = moment(date).add(14, 'days').format('YYYY-MM-DD');
+      return {
+        predictedPeriod,
+        predictedOvulation,
+      };
+    }
+    return null;
+  }
+
   const filterByPeriod = (data) => {
     const marked = {};
     const filteredData = data.filter((item) => item.period && item.period.menstrualFlow !== '');
     filteredData.forEach((item) => {
       const formatDate = moment(item.date, 'MM/DD/YYYY').format('YYYY-MM-DD');
+      const ovulationDate = predictOvulationNextPeriod(
+        metabolicJournal,
+        item.date
+      ).predictedOvulation;
+      const nextPeriodDate = predictOvulationNextPeriod(
+        metabolicJournal,
+        item.date
+      ).predictedPeriod;
       marked[formatDate] = {
         selected: true,
         selectedColor: '#FF647F',
       };
+      marked[ovulationDate] = {
+        selected: true,
+        selectedColor: 'yellow',
+      };
+      marked[nextPeriodDate] = {
+        selected: true,
+        selectedColor: 'blue',
+      };
     });
     return marked;
   };
+
+  // function predictOvulation(cycleLength) {
+  //   let predictedOvulation = cycleLength - 14;
+  //   if (cycleLength < 28) {
+  //     predictedOvulation -= 28 - cycleLength;
+  //   } else if (cycleLength > 28) {
+  //     predictedOvulation += cycleLength - 28;
+  //   }
+  //   return predictedOvulation;
+  // }
+
+  // Based on the first date of the period for a 28 day cycle, predict the ovulation date, and the date of the next period.
+  //   1. When a period is recorded, check if the previous date had recorded a period, and if it did not then set the current date as Day 1 of the cycle.
+  //  2. Based on Day 1, calculate the predicted Day 1 of the next cycle, and the ovulation date.
+
   const filterByUnprotectedSex = (data) => {
     const marked = {};
     const filteredData = data.filter((item) => item.sex.includes('Unprotected Sex'));
