1a5286d3773ebc35abbb4ea3fa2efe7a4edf0bcf
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-01 14:49:34 -0600

    journal format

--- screens/metabolicJournal/categories/Journal.js
+++ screens/metabolicJournal/categories/Journal.js
@@ -6,24 +6,24 @@ function Journal({ metabolicData, setMetabolicData, navigation }) {
   return (
     <View
       style={{
-        alignItems: 'flex-start',
+        alignItems: 'center',
         flex: 1,
         margin: '2%',
       }}
     >
-      <View style={{ flex: 1 }}>
-        <Text h3>Journal</Text>
-        <View style={{ flex: 1 }}>
-          <Button
-            title="Open Journal"
-            onPress={() => {
-              navigation.navigate('OpenJournal', {
-                metabolicData,
-                setMetabolicData,
-              });
-            }}
-          />
-        </View>
+      <Text h4 h4Style={{ color: 'black', alignSelf: 'flex-start' }}>
+        Journal
+      </Text>
+      <View style={{ alignItems: 'center' }}>
+        <Button
+          title="Open Journal"
+          onPress={() => {
+            navigation.navigate('OpenJournal', {
+              metabolicData,
+              setMetabolicData,
+            });
+          }}
+        />
       </View>
     </View>
   );
