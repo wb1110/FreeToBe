41e48e1935fca99ce0c5b29304930a7ef7af3445
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-01 14:46:59 -0600

    sleep changes

--- screens/metabolicJournal/PinnedComponent.js
+++ screens/metabolicJournal/PinnedComponent.js
@@ -48,30 +48,54 @@ function PinnedComponent({ metabolicData, setMetabolicData, navigation }) {
         </TouchableOpacity>
 
         <TouchableOpacity
-          style={{ alignItems: 'center', flex: 1 }}
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1,
+            backgroundColor:
+              selectedComponent === 'Sleep' ? theme.colors.white : theme.colors.primary,
+          }}
           onPress={() => setSelectedComponent('Sleep')}
         >
-          <Text h4 style={{ padding: '4%' }}>
+          <Text
+            h4
+            h4Style={{
+              padding: '4%',
+              display: selectedComponent === 'Sleep' ? 'none' : 'flex',
+            }}
+          >
             Sleep
           </Text>
           <MaterialCommunityIcons
             name="power-sleep"
             size={48}
-            color="white"
+            color={selectedComponent === 'Sleep' ? theme.colors.primary : 'white'}
             style={{ padding: '4%' }}
           />
         </TouchableOpacity>
         <TouchableOpacity
-          style={{ alignItems: 'center', flex: 1 }}
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1,
+            backgroundColor:
+              selectedComponent === 'Journal' ? theme.colors.white : theme.colors.primary,
+          }}
           onPress={() => setSelectedComponent('Journal')}
         >
-          <Text h4 style={{ padding: '4%' }}>
+          <Text
+            h4
+            h4Style={{
+              padding: '4%',
+              display: selectedComponent === 'Journal' ? 'none' : 'flex',
+            }}
+          >
             Journal
           </Text>
           <MaterialCommunityIcons
             name="notebook"
             size={48}
-            color="white"
+            color={selectedComponent === 'Journal' ? theme.colors.primary : 'white'}
             style={{ padding: '4%' }}
           />
         </TouchableOpacity>
@@ -82,14 +106,18 @@ function PinnedComponent({ metabolicData, setMetabolicData, navigation }) {
         </View>
       ) : null}
       {selectedComponent === 'Journal' ? (
-        <Journal
-          metabolicData={metabolicData}
-          setMetabolicData={setMetabolicData}
-          navigation={navigation}
-        />
+        <View style={{ backgroundColor: theme.colors.white }}>
+          <Journal
+            metabolicData={metabolicData}
+            setMetabolicData={setMetabolicData}
+            navigation={navigation}
+          />
+        </View>
       ) : null}
       {selectedComponent === 'Sleep' ? (
-        <Sleep metabolicData={metabolicData} setMetabolicData={setMetabolicData} />
+        <View style={{ backgroundColor: theme.colors.white }}>
+          <Sleep metabolicData={metabolicData} setMetabolicData={setMetabolicData} />
+        </View>
       ) : null}
     </View>
   );
--- screens/metabolicJournal/categories/Sleep.js
+++ screens/metabolicJournal/categories/Sleep.js
@@ -31,107 +31,96 @@ export default function Sleep({ metabolicData, setMetabolicData }) {
   return (
     <View
       style={{
-        alignItems: 'flex-start',
+        alignItems: 'center',
         flex: 1,
         margin: '2%',
       }}
     >
-      <View>
-        <Text h3>Sleep</Text>
-        <View>
-          <Text h4>Total Hours Slept: {total}</Text>
-          <CreateSleepEntry metabolicData={metabolicData} setMetabolicData={setMetabolicData} />
-          <View style={{ margin: '2%' }}>
-            {metabolicData.sleep
-              ? metabolicData.sleep.map((item, index) => {
-                  const time1 = moment(item.startTime, 'h:mm a');
-                  const time2 = moment(item.endTime, 'h:mm a');
-                  const difference = time2.diff(time1, 'hours');
-                  return (
-                    <View
-                      key={item.id}
-                      style={{
-                        alignItems: 'flex-end',
-                        backgroundColor: theme.colors.primary,
-                        padding: '2%',
-                        marginBottom: '2%',
-                        flex: 1,
+      <Text h4 h4Style={{ color: 'black', alignSelf: 'flex-start' }}>
+        Sleep
+      </Text>
+      <View style={{ alignItems: 'center' }}>
+        <Text h4 h4Style={{ color: 'black' }}>
+          Total Hours Slept: {total}
+        </Text>
+        <CreateSleepEntry metabolicData={metabolicData} setMetabolicData={setMetabolicData} />
+        <View style={{ margin: '2%', flexDirection: 'row' }}>
+          {metabolicData.sleep
+            ? metabolicData.sleep.map((item, index) => {
+                const time1 = moment(item.startTime, 'h:mm a');
+                const time2 = moment(item.endTime, 'h:mm a');
+                const difference = time2.diff(time1, 'hours');
+                return (
+                  <View
+                    key={item.id}
+                    style={{
+                      alignItems: 'flex-end',
+                      backgroundColor: theme.colors.primary,
+                      padding: '2%',
+                      marginLeft: '2%',
+                      flex: 1,
+                    }}
+                  >
+                    <View style={{ flex: 1, width: '100%' }}>
+                      <Text>Start Time: {moment(item.startTime).format('h:mm a')}</Text>
+                      <Text>End Time: {moment(item.endTime).format('h:mm a')}</Text>
+                      <Text>Hours Slept: {difference}</Text>
+                    </View>
+                    <View style={{ flex: 1, paddingRight: '2%' }}>
+                      <FontAwesome5
+                        name="ellipsis-h"
+                        size={16}
+                        color="white"
+                        onPress={() => setModalOpen(!modalOpen)}
+                      />
+                    </View>
+
+                    <Modal
+                      animationType="slide"
+                      transparent
+                      visible={modalOpen}
+                      onRequestClose={() => {
+                        setModalOpen(!modalOpen);
                       }}
                     >
-                      <View style={{ flex: 1, width: '100%' }}>
-                        <Text>Start Time: {moment(item.startTime).format('h:mm a')}</Text>
-                        <Text>End Time: {moment(item.endTime).format('h:mm a')}</Text>
-                        <Text>Hours Slept: {difference}</Text>
-                      </View>
-                      <View style={{ flex: 1, paddingRight: '2%' }}>
-                        <FontAwesome5
-                          name="ellipsis-h"
-                          size={16}
-                          color="white"
-                          onPress={() => setModalOpen(!modalOpen)}
-                        />
-                      </View>
-
-                      <Modal
-                        animationType="slide"
-                        transparent
-                        visible={modalOpen}
-                        onRequestClose={() => {
-                          setModalOpen(!modalOpen);
-                        }}
+                      <TouchableOpacity
+                        style={styles.container}
+                        activeOpacity={1}
+                        onPressOut={() => setModalOpen(!modalOpen)}
                       >
-                        <TouchableOpacity
-                          style={styles.container}
-                          activeOpacity={1}
-                          onPressOut={() => setModalOpen(!modalOpen)}
-                        >
-                          <TouchableWithoutFeedback>
-                            <View style={styles.menuContainer}>
-                              <View style={styles.innerMenu}>
-                                {/* <TouchableOpacity
-                                  style={{
-                                    padding: '5%',
-                                    borderBottomColor: 'white',
-                                    borderBottomWidth: 1,
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                  }}
-                                  onPress={() => console.log('edit')}
-                                >
-                                  <Feather name="edit-2" size={24} color={theme.colors.white} />
-                                  <Text style={{ marginLeft: '2%' }}>Edit Sleep Session</Text>
-                                </TouchableOpacity> */}
-                                <TouchableOpacity
-                                  style={{
-                                    padding: '5%',
-                                    borderBottomColor: 'white',
-                                    borderBottomWidth: 1,
-                                    flexDirection: 'row',
-                                    alignItems: 'center',
-                                  }}
-                                  onPress={() => {
-                                    metabolicData.sleep.splice(index, 1);
-                                    calculateTotal(metabolicData.sleep);
-                                    setModalOpen(!modalOpen);
-                                  }}
-                                >
-                                  <MaterialIcons
-                                    name="delete-outline"
-                                    size={24}
-                                    color={theme.colors.white}
-                                  />
-                                  <Text style={{ marginLeft: '2%' }}>Delete Sleep Session</Text>
-                                </TouchableOpacity>
-                              </View>
+                        <TouchableWithoutFeedback>
+                          <View style={styles.menuContainer}>
+                            <View style={styles.innerMenu}>
+                              <TouchableOpacity
+                                style={{
+                                  padding: '5%',
+                                  borderBottomColor: 'white',
+                                  borderBottomWidth: 1,
+                                  flexDirection: 'row',
+                                  alignItems: 'center',
+                                }}
+                                onPress={() => {
+                                  metabolicData.sleep.splice(index, 1);
+                                  calculateTotal(metabolicData.sleep);
+                                  setModalOpen(!modalOpen);
+                                }}
+                              >
+                                <MaterialIcons
+                                  name="delete-outline"
+                                  size={24}
+                                  color={theme.colors.white}
+                                />
+                                <Text style={{ marginLeft: '2%' }}>Delete Sleep Session</Text>
+                              </TouchableOpacity>
                             </View>
-                          </TouchableWithoutFeedback>
-                        </TouchableOpacity>
-                      </Modal>
-                    </View>
-                  );
-                })
-              : null}
-          </View>
+                          </View>
+                        </TouchableWithoutFeedback>
+                      </TouchableOpacity>
+                    </Modal>
+                  </View>
+                );
+              })
+            : null}
         </View>
       </View>
     </View>
