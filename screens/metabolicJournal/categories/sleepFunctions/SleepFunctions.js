04aeec67350c13f2b0a2117076843dfdd4f7e1b7
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-02 08:03:07 -0600

    sleep calculation is working

--- screens/metabolicJournal/categories/sleepFunctions/SleepFunctions.js
+++ screens/metabolicJournal/categories/sleepFunctions/SleepFunctions.js
@@ -5,9 +5,13 @@ const calculateTotal = (sleepData) => {
   sleepData.forEach((item) => {
     const time1 = moment(item.startTime, 'h:mm a');
     const time2 = moment(item.endTime, 'h:mm a');
-    newTotal += time2.diff(time1, 'hours');
+    newTotal += time2.diff(time1, 'minutes');
   });
-  return newTotal;
+  const hours = newTotal / 60;
+  const rhours = Math.floor(hours);
+  const minutes = (hours - rhours) * 60;
+  const rminutes = Math.round(minutes);
+  return `${rhours}h ${rminutes}min`;
 };
 
 export default calculateTotal;
