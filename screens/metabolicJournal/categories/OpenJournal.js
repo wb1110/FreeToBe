833cf00727cd7e5b720135ab2270b6b15ce16665
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-02 11:18:22 -0600

    temp fix for journal

--- screens/metabolicJournal/categories/OpenJournal.js
+++ screens/metabolicJournal/categories/OpenJournal.js
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
-import { Platform, KeyboardAvoidingView, SafeAreaView, ScrollView, View } from 'react-native';
-import { Text } from '@rneui/themed';
+import React, { useEffect, useState } from 'react';
+import {
+  Platform,
+  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Keyboard,
+} from 'react-native';
+import { Button, Text } from '@rneui/themed';
 import { actions, RichEditor, RichToolbar } from 'react-native-pell-rich-editor';
 import LArrowButton from '../../../components/Buttons/LArrowButton';
 
 function OpenJournal({ route, navigation }) {
   const { metabolicData, setMetabolicData } = route.params;
   const richText = React.useRef();
+
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
-      <Text h3>Journal</Text>
-      <ScrollView style={{ flex: 1, width: '100%' }}>
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text h3>Journal</Text>
+        <Button
+          title="Done"
+          type="clear"
+          containerStyle={{ position: 'absolute', right: 0 }}
+          titleStyle={{ color: 'white' }}
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+      <ScrollView keyboardShouldPersistTaps="handled" style={{ flex: 1, width: '100%' }}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
           <RichEditor
             ref={richText}
             initialContentHTML={metabolicData.journal}
@@ -25,26 +52,30 @@ function OpenJournal({ route, navigation }) {
           />
         </KeyboardAvoidingView>
       </ScrollView>
-
-      <RichToolbar
-        editor={richText}
-        actions={[
-          actions.setBold,
-          actions.setItalic,
-          actions.insertBulletsList,
-          actions.insertOrderedList,
-          actions.insertLink,
-          actions.setStrikethrough,
-          actions.setUnderline,
-          actions.checkboxList,
-          actions.undo,
-          actions.redo,
-        ]}
-        iconMap={{
-          [actions.heading1]: ({ tintColor }) => <Text style={[{ color: tintColor }]}>H1</Text>,
-        }}
-        style={{ width: '100%' }}
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{ width: '100%', flex: 1 }}
+      >
+        <RichToolbar
+          editor={richText}
+          actions={[
+            actions.setBold,
+            actions.setItalic,
+            actions.insertBulletsList,
+            actions.insertOrderedList,
+            actions.insertLink,
+            actions.setStrikethrough,
+            actions.setUnderline,
+            actions.checkboxList,
+            actions.undo,
+            actions.redo,
+          ]}
+          iconMap={{
+            [actions.heading1]: ({ tintColor }) => <Text style={[{ color: tintColor }]}>H1</Text>,
+          }}
+          style={{ width: '100%' }}
+        />
+      </KeyboardAvoidingView>
       <LArrowButton onPress={() => navigation.goBack()} />
     </SafeAreaView>
   );
