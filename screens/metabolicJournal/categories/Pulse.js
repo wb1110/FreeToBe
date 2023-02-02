18d7bacc5b10a38371ac055f711a17bd464e22ec
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-02 08:49:29 -0600

    pulse format

--- screens/metabolicJournal/categories/Pulse.js
+++ screens/metabolicJournal/categories/Pulse.js
@@ -1,8 +1,13 @@
 /* eslint-disable global-require */
-import { Input, Text } from '@rneui/themed';
+import { Button, Input, Overlay, Text } from '@rneui/themed';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 export default function Pulse({ metabolicData, setMetabolicData }) {
+  const [visible, setVisible] = useState(false);
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
   return (
     <View
       style={{
@@ -14,15 +19,24 @@ export default function Pulse({ metabolicData, setMetabolicData }) {
       <View style={{ flex: 1 }}>
         <Text h3>Pulse</Text>
         <View style={{ flex: 1 }}>
-          <Input
-            label="Enter morning pulse here"
-            onChangeText={(value) =>
-              setMetabolicData({
-                ...metabolicData,
-                pulse: value,
-              })
-            }
+          <Button
+            title="Set Waking Temp"
+            onPress={() => toggleOverlay('waking')}
+            containerStyle={{ borderRadius: 20, width: 200 }}
           />
+          <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+            <Input
+              label="Enter morning pulse here"
+              onChangeText={(value) =>
+                setMetabolicData({
+                  ...metabolicData,
+                  pulse: value,
+                })
+              }
+              labelStyle={{ color: 'black' }}
+              style={{ borderColor: 'black', borderWidth: 1, borderRadius: 20, color: 'black' }}
+            />
+          </Overlay>
         </View>
       </View>
     </View>
