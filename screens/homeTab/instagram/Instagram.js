f9bf9f4df226904c073b1c8a179238eb99bd63d7
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-08 15:09:58 -0600

    instagram feed working

--- screens/homeTab/instagram/Instagram.js
+++ screens/homeTab/instagram/Instagram.js
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Image } from '@rneui/themed';
 import { INSTA_ACCESS } from '@env';
+import Swiper from 'react-native-swiper';
 
 export default function Instagram() {
   const [data, setData] = useState();
   useEffect(() => {
     axios
       .get(
-        `https://graph.instagram.com/me/media?fields=media_url,caption,permalink,timestamp,thumbnail_url&access_token=${INSTA_ACCESS}`
+        `https://graph.instagram.com/me/media?fields=media_url,permalink,timestamp,thumbnail_url,media_type&access_token=${INSTA_ACCESS}`
       )
       .then((res) => {
         setData(res.data.data.slice(0, 4));
@@ -20,17 +21,36 @@ export default function Instagram() {
   }, []);
 
   return (
-    <View>
-      {data
-        ? data.map((item, index) => (
-            <View key={index}>
-              <Image
-                source={{ uri: item.thumbnail_url }}
-                style={{ width: '100%', aspectRatio: 1, flex: 1 }}
-              />
-            </View>
-          ))
-        : null}
-    </View>
+    <Swiper style={{ height: 300 }} activeDotColor="white">
+      {data ? (
+        data.map((item, index) => (
+          <View
+            key={index}
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              aspectRatio: 1,
+            }}
+          >
+            <Image
+              source={{ uri: item.media_type === 'VIDEO' ? item.thumbnail_url : item.media_url }}
+              style={{ width: '100%', aspectRatio: 1, flex: 1 }}
+            />
+          </View>
+        ))
+      ) : (
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            aspectRatio: 1,
+          }}
+        >
+          <Text>Insta not working</Text>
+        </View>
+      )}
+    </Swiper>
   );
 }
