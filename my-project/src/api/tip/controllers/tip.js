dd79bbbe492c486c34bfe3624cba2384edf005aa
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-22 15:57:35 -0600

    moved CMS

--- my-project/src/api/tip/controllers/tip.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tip controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::tip.tip');
--- my-project/src/api/tip/routes/tip.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tip router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::tip.tip');
