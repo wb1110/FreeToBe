df17524746f62d5d3a9ff47b8781eedde800200d
wb1110 <williamjaredbuchanan@gmail.com>
2023-02-22 10:54:02 -0600

    energy format

--- screens/settings/targets/Energy.js
+++ screens/settings/targets/Energy.js
@@ -57,55 +57,63 @@ export default function Energy({ navigation }) {
       }}
     >
       <LArrowButton onPress={() => navigation.goBack()} />
-      <Text h4 h4Style={{ margin: 16 }}>
-        Energy Burned
-      </Text>
-      <View
-        style={{
-          justifyContent: 'center',
-          backgroundColor: theme.colors.secondary,
-          borderRadius: 25,
-        }}
-      >
-        <View style={styles.container}>
-          <Text h4>BMR</Text>
-          <Text h4>{bmr()} kcal</Text>
-        </View>
-        <View style={{ padding: 16, borderBottomWidth: 1, borderBottomColor: 'white' }}>
-          <Text h4>Activity Level</Text>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginTop: 16,
-            }}
-          >
-            <Text h4>At work</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <WorkActivitySelector />
-            </View>
-          </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              marginTop: 16,
-            }}
-          >
-            <Text h4>Outside work</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <ExerciseActivitySelector />
-            </View>
-          </View>
-        </View>
+      <View style={{ flex: 1, justifyContent: 'center' }}>
         <View
           style={{
-            alignItems: 'flex-end',
-            padding: 16,
+            marginBottom: 24,
           }}
         >
-          <Text>Total Energy Burned (TDEE)</Text>
-          <Text h4> = {assessment.tdee}</Text>
+          <Text h4 h4Style={{ margin: 16 }}>
+            Energy Burned
+          </Text>
+          <View
+            style={{
+              justifyContent: 'center',
+              backgroundColor: theme.colors.secondary,
+              borderRadius: 25,
+            }}
+          >
+            <View style={styles.container}>
+              <Text h4>BMR</Text>
+              <Text h4>{bmr()} kcal</Text>
+            </View>
+            <View style={{ padding: 16, borderBottomWidth: 1, borderBottomColor: 'white' }}>
+              <Text h4>Activity Level</Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  marginTop: 16,
+                }}
+              >
+                <Text h4>At work</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <WorkActivitySelector />
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  marginTop: 16,
+                }}
+              >
+                <Text h4>Outside work</Text>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <ExerciseActivitySelector />
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                alignItems: 'flex-end',
+                padding: 16,
+              }}
+            >
+              <Text>Total Energy Burned (TDEE)</Text>
+              <Text h4> = {assessment.tdee}</Text>
+            </View>
+          </View>
         </View>
       </View>
     </View>
